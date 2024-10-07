4f3fff32f1649781e5be015a01861b1026c4d942
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-08 00:03:52 +0200

    printed on page $user_random_password on session.php

--- session.php
+++ session.php
@@ -3,3 +3,34 @@
 session_start();
 var_dump($_SESSION);
 $user_random_password = $_SESSION['user_random_password'];
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Melvin Jerome Luna Maligaya">
+    <title>php-strong-password-generator</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class=" py-5">
+        <div class="container mt-4">
+            <h1 class="fw-bolder mb-3">This is your new password:</h1>
+            <h3 class="fw-bolder ps-3 text-success"><?php echo $user_random_password ?></h3>
+        </div>
+    </main>
+
+    <!-- js bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
